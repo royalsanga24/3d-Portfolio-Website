78e24fa90962492e6a6dbcc09366aa83f8cd5800
royalsanga24 <royalsanga24@gmail.com>
2024-01-25 19:47:26 +0530

    Contact Form First Commit

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      gl={{preserveDrawingBuffer:true}}
+      camera={{}}
+    >
+      <Suspense> fallback={<CanvasLoader />}
+        <OrbitControls 
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI/2}
+          minPolarAngle={Math.PI/2}
+        />
+      </Suspense>
+    </Canvas>
   )
 }
 
