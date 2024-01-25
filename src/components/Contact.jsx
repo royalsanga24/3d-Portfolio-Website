e7f2486105b0148c1f96c4fc854757d0fde87362
roy <you@example.com>
2024-01-25 21:51:11 +0530

    Contact Page Rotating Model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,7 @@ import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
 
 const Contact = () => {
+  const formRef = useRef();
   const [form, setForm] = useState({
     name: '',
     email: '',
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,8 +4,14 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf')
   return (
-    <div>Earth</div>
+    <primitive
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      position-x={0}
+    />
   )
 }
 
@@ -15,18 +21,25 @@ const EarthCanvas = () => {
       shadows
       frameloop='demand'
       gl={{preserveDrawingBuffer:true}}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
     >
-      <Suspense> fallback={<CanvasLoader />}
+      <Suspense fallback={<CanvasLoader />}> 
         <OrbitControls 
           autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI/2}
           minPolarAngle={Math.PI/2}
         />
+        <Earth />
+        <Preload all />
       </Suspense>
     </Canvas>
   )
 }
 
-export default Earth
+export default EarthCanvas
